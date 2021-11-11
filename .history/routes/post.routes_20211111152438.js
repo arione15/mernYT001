c272d75734bd31f53d1a10ba4b66f2c0d95fcec1
Massine <massinoueslati@gmail.com156>
2021-11-11 15:31:41 +0100

    comments : supprimer un commentaire

--- .history/routes/post.routes_20211111152438.js
+++ .history/routes/post.routes_20211111152438.js
@@ -13,4 +13,6 @@ router.patch('/comment-post/:id', postController.commentPost);
 router.patch('/edit-comment-post/:id', postController.editCommentPost);
 router.patch('/delete-comment-post/:id', postController.deleteCommentPost);
 
+
+
 module.exports = router;
